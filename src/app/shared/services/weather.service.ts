2c6de971f0f841f016d7acb1d5100d8a31e5fb4d
Marcos Januário <januario.marcos@icloud.com>
2023-12-04 08:39:49 +0100

    reading url api form dev/prod environment

--- src/app/shared/services/weather.service.ts
+++ src/app/shared/services/weather.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { WeatherResponseData } from '../types/Weather';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
-  private apiUrl = 'http://localhost:3000/weather';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
